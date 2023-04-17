8ee772cfdf78cb702ce63aac30c304d4733bad2c
LampTales <179500516@qq.com>
2023-04-17 15:59:13 +0800

    pc ctrl update

--- CPU.srcs/sources_1/new/PCctrl.v
+++ CPU.srcs/sources_1/new/PCctrl.v
@@ -19,6 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+// build doing
 
 module PCctrl(
     input [31:0] pc,
@@ -34,5 +35,47 @@ module PCctrl(
     output [31:0] link_addr,
     output [31:0] next
     );
-    
+    wire [31:0] pc_add_4_dest;
+    assign pc_add_4_dest = pc + 4;
+    assign link_addr = pc_add_4_dest;
+
+    wire [31:0] j_dest;
+    reg [31:0] zero;
+    assign j_dest = {pc[31:28], j_inst[25:0], zero[1:0]};
+
+    wire [31:0] branch_dest;
+    assign branch_dest = pc_add_4_dest + {expand_imme[29:0], zero[1:0]};
+
+    wire pcadd4_branch_select;
+    assign pcadd4_branch_select = (equal & branch) | ((~equal) & nbranch);
+    wire [31:0] mux0_out;
+    MUX_32 pcadd4_or_branch(
+        .in0(pc_add_4_dest),
+        .in1(branch_dest),
+        .select(pcadd4_branch_select),
+        .out(mux0_out)
+    );
+
+    wire mux0_jump_select;
+    assign mux0_jump_select = j | jal;
+    wire [31:0] mux1_out;
+    MUX_32 mux0_or_jump(
+        .in0(mux0_out),
+        .in1(j_dest),
+        .select(mux0_jump_select),
+        .out(mux1_out)
+    );
+
+    wire mux2_jr_select;
+    assign mux2_jr_select = jr;
+    wire [31:0] mux2_out;
+    MUX_32 mux2_or_jr(
+        .in0(mux1_out),
+        .in1(ra),
+        .select(mux2_jr_select),
+        .out(mux2_out)
+    );
+
+    assign next = mux2_out;
+
 endmodule
