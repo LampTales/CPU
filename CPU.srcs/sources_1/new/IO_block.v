539520f91a5911befbd5a3c39c0977fc5af7ba69
LampTales <179500516@qq.com>
2023-05-17 17:56:40 +0800

    hardware signal

--- CPU.srcs/sources_1/new/IO_block.v
+++ CPU.srcs/sources_1/new/IO_block.v
@@ -31,12 +31,16 @@ module IO_block(
     input cpu_rst_butt,
     input mode_butt,
     input ack_butt,
-    output [1:0] mode,
+    output cpu_rst,
+    output reg [1:0] mode,
     output ack,
     output [7:0] board_input_data,
     output [3:0] board_input_case,
     input [31:0] board_output_data,
     input [7:0] board_output_sig
+
+    // the rest is hardware signal
+    
     );
 
     seg_block seg(
@@ -47,6 +51,42 @@ module IO_block(
         .seg_out(seg_out)
     );
 
+    button cpu_rst_butt_antishake(
+        .clk(pc_clk),
+        .rst_n(rst),
+        .input_button(cpu_rst_butt),
+        .output_button(cpu_rst)
+    );
+
+    wire mode_change;
+    button mode_butt_antishake(
+        .clk(pc_clk),
+        .rst_n(rst),
+        .input_button(mode_butt),
+        .output_button(mode_change)
+    );
+
+    always@(posedge mode_change or negedge rst) begin
+        if(!rst) begin
+            mode <= 2'b00;  // ori
+        end
+        else begin
+            case(mode)
+                2'b00: mode <= 2'b01;   // uart
+                2'b01: mode <= 2'b10;   // run
+                2'b10: mode <= 2'b11;   // debug
+                2'b11: mode <= 2'b00;   //back to ori
+            endcase
+            end
+    end
+
+
+    button ack_butt_antishake(
+        .clk(pc_clk),
+        .rst_n(rst),
+        .input_button(ack_butt),
+        .output_button(ack)
+    );
 
     
 
