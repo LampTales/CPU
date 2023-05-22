a3a92867b55041495cb791a5235e42f95c80a281
LampTales <179500516@qq.com>
2023-05-22 12:14:29 +0800

    reg

--- CPU.srcs/sources_1/new/REG.v
+++ CPU.srcs/sources_1/new/REG.v
@@ -42,21 +42,50 @@ module REG(
     always @(posedge clk or negedge rst) begin
         if (!rst) begin
             save[31:0] <= 0; // zero
-            save[29*32+31:29*32] <= 2'b00000000000000001111111100000000;
+            save[959:928] <= 2'b00000000000000001111111100000000;
 
         end
-        else begin
-            if (reg_write) begin
-                save[write*32+31:write*32] <= write_data;
-            end 
+        else if(reg_write) begin
+            case(write)
+                3: save[127:96] <= write_data;
+                4: save[159:128] <= write_data;
+                5: save[191:160] <= write_data;
+                6: save[223:192] <= write_data;
+                7: save[255:224] <= write_data;
+                8: save[287:256] <= write_data;
+                9: save[319:288] <= write_data;
+                10: save[351:320] <= write_data;
+                11: save[383:352] <= write_data;
+                12: save[415:384] <= write_data;
+                13: save[447:416] <= write_data;
+                14: save[479:448] <= write_data;
+                15: save[511:480] <= write_data;
+                16: save[543:512] <= write_data;
+                17: save[575:544] <= write_data;
+                18: save[607:576] <= write_data;
+                19: save[639:608] <= write_data;
+                20: save[671:640] <= write_data;
+                21: save[703:672] <= write_data;
+                22: save[735:704] <= write_data;
+                23: save[767:736] <= write_data;
+                24: save[799:768] <= write_data;
+                25: save[831:800] <= write_data;
+                26: save[863:832] <= write_data;
+                27: save[895:864] <= write_data;
+                28: save[927:896] <= write_data;
+                29: save[959:928] <= write_data;
+                30: save[991:960] <= write_data;
+                31: save[1023:992] <= write_data;
+                default: save[31:0] <= 0;
+            endcase
         end
     end
 
     // reg read
-    // ack set to be $v0
-    always @(read0, read1) begin
-        read_data0 = save[read0*32+31:read0*32];
-        read_date1 = save[read1*32+31:read1*32];
+    always @(read0) begin
+        case(read0)
+            
+        endcase
     end
 
 endmodule
