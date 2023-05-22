129b7c06785e4b85000931699d01ddf26023fd01
LampTales <179500516@qq.com>
2023-05-22 11:42:01 +0800

    reg test

--- CPU.srcs/sources_1/new/REG.v
+++ CPU.srcs/sources_1/new/REG.v
@@ -33,19 +33,30 @@ module REG(
     output [31:0] read_data0,
     output [31:0] read_data1,
     output [127:0] simd_read_data0,
-    output [127:0] simd_read_data1
+    output [127:0] simd_read_data1,
+    input ack
     );
     reg [1023:0] save;
 
+    // reg write
     always @(posedge clk or negedge rst) begin
         if (!rst) begin
             save[31:0] <= 0; // zero
             save[29*32+31:29*32] <= 2'b00000000000000001111111100000000;
 
         end
-
-        if (reg_write) begin
-            
+        else begin
+            if (reg_write) begin
+                save[write*32+31:write*32] <= write_data;
+            end 
         end
     end
+
+    // reg read
+    // ack set to be $v0
+    always @(read0, read1) begin
+        read_data0 = save[read0*32+31:read0*32];
+        read_date1 = save[read1*32+31:read1*32];
+    end
+
 endmodule
