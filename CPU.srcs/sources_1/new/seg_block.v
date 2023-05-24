afa694d8506aeead4208afb1e80b234492d199a1
wyr1122 <wyr321321@126.com>
2023-05-24 20:25:52 +0800

    SEG

--- CPU.srcs/sources_1/new/seg_block.v
+++ CPU.srcs/sources_1/new/seg_block.v
@@ -35,18 +35,20 @@ module seg_block(input clk,
     reg [2:0] cnt;
     reg [3:0] hexadecimals [7:0];
     reg [3:0] current_hex;
-    reg[7:0] temp;
+    reg[6:0] temp;
     
     always @ (seg_in) begin
-        if (seg_in[31:24] = 8'hff)begin
-            temp            = seg_in[15:0];
+        if (seg_in[15:8] = 8'hff)begin
+            temp            = seg_in[6:0] ;
+            if(seg_in[7]begin
+                temp = ~temp +1
+            end)
             hexadecimals[0] = temp%10;
             temp            = temp/10;
             hexadecimals[1] = temp%10;
             temp            = temp/10;
             hexadecimals[2] = temp%10;
-            temp            = temp/10;
-            hexadecimals[3] = temp%10;
+            hexadecimals[3] = seg_in[7]?4'hf:0;
         end
         else begin
             hexadecimals[0] = seg_in[3:0];
@@ -56,14 +58,16 @@ module seg_block(input clk,
         end
         
         if (seg_in[31:24] = 8'hff)begin
-            temp            = seg_in[23:16];
+            temp            = seg_in[22:16];
+            if(seg_in[23]begin
+                temp = ~temp +1
+            end)
             hexadecimals[4] = temp%10;
             temp            = temp/10;
             hexadecimals[5] = temp%10;
             temp            = temp/10;
             hexadecimals[6] = temp%10;
-            temp            = temp/10;
-            hexadecimals[7] = temp%10;
+            hexadecimals[7] = seg_in[23]?4'hf:0;
         end
         else begin
             hexadecimals[4] = seg_in[19:16];
@@ -85,17 +89,17 @@ module seg_block(input clk,
     always @ (cnt) begin
         case(cnt)
         0: begin seg_op <= 8'b1111_1110; current_hex <= hexadecimals[0]; end
-    1: begin seg_op <= 8'b1111_1101; current_hex <= hexadecimals[1]; end
-2: begin seg_op <= 8'b1111_1011; current_hex <= hexadecimals[2]; end
-3: begin seg_op <= 8'b1111_0111; current_hex <= hexadecimals[3]; end
-4: begin seg_op <= 8'b1110_1111; current_hex <= hexadecimals[4]; end
-5: begin seg_op <= 8'b1101_1111; current_hex <= hexadecimals[5]; end
-6: begin seg_op <= 8'b1011_1111; current_hex <= hexadecimals[6]; end
-7: begin seg_op <= 8'b0111_1111; current_hex <= hexadecimals[7]; end
-default: begin seg_op <= 8'b1111_1111; current_hex <= 0; end
+        1: begin seg_op <= 8'b1111_1101; current_hex <= hexadecimals[1]; end
+        2: begin seg_op <= 8'b1111_1011; current_hex <= hexadecimals[2]; end
+        3: begin seg_op <= 8'b1111_0111; current_hex <= hexadecimals[3]; end
+        4: begin seg_op <= 8'b1110_1111; current_hex <= hexadecimals[4]; end
+        5: begin seg_op <= 8'b1101_1111; current_hex <= hexadecimals[5]; end
+        6: begin seg_op <= 8'b1011_1111; current_hex <= hexadecimals[6]; end
+        7: begin seg_op <= 8'b0111_1111; current_hex <= hexadecimals[7]; end
+        default: begin seg_op <= 8'b1111_1111; current_hex <= 0; end
         endcase
     end
-    
+   //TODO FIX SEG_OUT 
     always @ (current_hex) begin
         case (current_hex)
             4'h0: seg_out    = 8'b01000000; // 0
@@ -113,7 +117,7 @@ default: begin seg_op <= 8'b1111_1111; current_hex <= 0; end
             4'hc: seg_out    = 8'b01000110; // c
             4'hd: seg_out    = 8'b00100001; // d
             4'he: seg_out    = 8'b00000110; // E
-            4'hf: seg_out    = 8'b00001110; // F
+            4'hf: seg_out    = 8'b00001110; // -
             default: seg_out = 8'b0000000;
         endcase
     end
