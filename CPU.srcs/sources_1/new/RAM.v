4d74fab7339c7210da8afdbfe92ab3cada193dd8
LampTales <179500516@qq.com>
2023-05-28 13:59:14 +0800

    only ram changed

--- CPU.srcs/sources_1/new/RAM.v
+++ CPU.srcs/sources_1/new/RAM.v
@@ -45,30 +45,61 @@ module RAM(input clk,
     assign board_input_data = in_num;
     wire [31:0] board_input_case;
     assign board_input_case = in_case;
+
+
+    // always@(*) begin
+    //     casex(addr)
+    //         32'b00000000000000000011111111110000: read_data = board_input_data;
+    //         32'b00000000000000000011111111110100: read_data = board_input_case;
+    //         default: read_data                              = ram_out;
+    //     endcase
+    // end
+    
+    // reg [31:0] inner_addr;
+
+    // with star
     always@(*) begin
-        casex(addr)
-            32'b00000000000000000011111111110000: read_data = board_input_data;
-            32'b00000000000000000011111111110100: read_data = board_input_case;
-            default: read_data                              = ram_out;
+        casex(addr[15:2])
+            14'b00111111111100: read_data = board_input_data;
+            14'b00111111111101: read_data = board_input_case;
+            default: read_data = ram_out;
         endcase
     end
+
     
     reg [31:0] board_output_data;
     assign out_num = board_output_data;
     reg [31:0] board_output_sig;
     assign out_sig = board_output_sig;
     
-    always@(*) begin
+
+    // always@(*) begin
+    //     if (!rst) begin
+    //         board_output_data = 0;
+    //         board_output_sig  = 0;
+    //     end
+    //     else begin
+    //     casex(addr)
+    //         32'b0000 0000 0000 0000 0011111111111000: board_output_data = (mem_write) ? write_data : board_output_data;
+    //         32'b00000000000000000011111111111100: board_output_sig  = (mem_write) ? write_data : board_output_sig;
+    //     endcase
+    //     end
+    // end
+
+    // now it work with the inner_ram
+    always @(posedge clk or negedge rst) begin
         if (!rst) begin
-            board_output_data = 0;
-            board_output_sig  = 0;
+            board_output_data <= 0;
+            board_output_sig  <= 0;
         end
-        else begin
-        casex(addr)
-            32'b00000000000000000011111111111000: board_output_data = (mem_write) ? write_data : board_output_data;
-            32'b00000000000000000011111111111100: board_output_sig  = (mem_write) ? write_data : board_output_sig;
+        else if (mem_write) begin
+        case (addr[15:2])
+            14'b00111111111110: board_output_data <= write_data;
+            14'b00111111111111: board_output_sig <= write_data;
         endcase
         end
+
+        
     end
 
 endmodule
